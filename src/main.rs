430769b072bd8ae2fb850e46a72df19dc1e9ba78
Adam Waselnuk <adam.waselnuk@gmail.com>
2023-03-26 17:34:56 -0400

    Extract player to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
-use std::cmp::{max, min};
 
 mod map;
 pub use map::*;
+mod player;
+pub use player::*;
 
 // COMPONENTS
 #[derive(Component)]
@@ -47,41 +48,13 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
-struct State {
+pub struct State {
     ecs: World,
 }
 
 impl State {
     fn run_systems(&mut self) {
         self.ecs.maintain();
-    }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {} // Unhandled key
-        },
     }
 }
 
