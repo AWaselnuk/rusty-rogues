546e02c212c749de69aff36563e687bb298c7b38
Adam Waselnuk <adam.waselnuk@gmail.com>
2023-03-26 17:40:43 -0400

    extract systems

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,6 @@ pub use map::*;
 mod player;
 pub use player::*;
 
-// SYSTEMS
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
 pub struct State {
     ecs: World,
 }
